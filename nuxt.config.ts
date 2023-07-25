575eef1bad35437620b0cfb83e4311eb8850af5a
Xenia Lu <xenia.lyy@gmail.com>
2023-07-25 14:30:09 +0800

    feat: update theme tokens & support tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,22 @@ export default defineNuxtConfig({
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible',
     // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
-  ]
+    '@nuxt/devtools',
+    // https://github.com/nuxt-modules/tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
+
+  tailwindcss: {
+    config: {
+      /* Extend the Tailwind config here. */
+      content: ['content/**/**.md', 'components/**/**.vue'],
+      theme: {
+        extend: {
+          colors: {
+            primary: '#215ae5'
+          }
+        }
+      }
+    }
+  }
 });
